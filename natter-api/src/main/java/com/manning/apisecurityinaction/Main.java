aa3b0c162aa6ff986540be1c143c87df8f009919
Neil Madden <neil.madden@forgerock.com>
2019-04-15 15:57:47 +0100

    Listing 2.5 Wiring up the Natter API endpoints

--- natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
@@ -1,9 +1,14 @@
 package com.manning.apisecurityinaction;
 
+import static spark.Spark.*;
+
 import java.nio.file.*;
 import java.sql.Connection;
 
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.JSONObject;
+
+import com.manning.apisecurityinaction.controller.SpaceController;
 
 public class Main {
 
@@ -11,6 +16,19 @@ public class Main {
         var datasource = JdbcConnectionPool.create(
             "jdbc:h2:mem:natter", "natter", "password");
         createTables(datasource.getConnection());
+
+        var spaceController =
+            new SpaceController(datasource);
+
+        post("/spaces", spaceController::createSpace);
+        afterAfter((request, response) -> {
+            response.type("application/json");
+        });
+
+        internalServerError(new JSONObject()
+            .put("error", "internal server error").toString());
+        notFound(new JSONObject()
+            .put("error", "not found").toString());
     }
 
     private static void createTables(Connection connection) throws Exception {
