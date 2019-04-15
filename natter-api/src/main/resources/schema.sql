d606a49b09ea83d09ea070e975642df7441be57f
Neil Madden <neil.madden@forgerock.com>
2019-04-15 15:45:25 +0100

    Listing 2.2 Add the database schema SQL file

---
+++ natter-api/src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE spaces(
+    space_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    owner VARCHAR(30) NOT NULL
+);
+CREATE SEQUENCE space_id_seq;
+CREATE TABLE messages(
+    space_id INT NOT NULL REFERENCES spaces(space_id),
+    msg_id INT PRIMARY KEY,
+    author VARCHAR(30) NOT NULL,
+    msg_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    msg_text VARCHAR(1024) NOT NULL
+);
+CREATE SEQUENCE msg_id_seq;
+CREATE INDEX msg_timestamp_idx ON messages(msg_time);
+CREATE UNIQUE INDEX space_name_idx ON spaces(name);
