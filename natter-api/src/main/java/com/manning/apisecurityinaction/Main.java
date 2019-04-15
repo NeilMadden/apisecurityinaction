4037d975d611c3a2b0b1fad98741a73a37d8416f
Neil Madden <neil.madden@forgerock.com>
2019-04-15 15:50:05 +0100

    Listing 2.3 Setting up the connection pool

---
+++ natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,26 @@
+package com.manning.apisecurityinaction;
+
+import java.nio.file.*;
+import java.sql.Connection;
+
+import org.h2.jdbcx.JdbcConnectionPool;
+
+public class Main {
+
+    public static void main(String... args) throws Exception {
+        var datasource = JdbcConnectionPool.create(
+            "jdbc:h2:mem:natter", "natter", "password");
+        createTables(datasource.getConnection());
+    }
+
+    private static void createTables(Connection connection) throws Exception {
+        try (var conn = connection;
+             var stmt = conn.createStatement()) {
+            conn.setAutoCommit(false);
+            Path path = Paths.get(
+                    Main.class.getResource("/schema.sql").toURI());
+            stmt.execute(Files.readString(path));
+            conn.commit();
+        }
+    }
+}
