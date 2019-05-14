eca74f1db4cf0bfdb1bebae167f491939b6c71a1
Neil Madden <neil.madden@forgerock.com>
2019-05-14 16:14:50 +0100

    Listing 4.1 Calling the Natter API from JavaScript

---
+++ natter-api/src/main/resources/public/natter.js
@@ -0,0 +1,22 @@
+const apiUrl = 'https://localhost:4567';
+
+function createSpace(name, owner) {
+    let data = {name: name, owner: owner};
+
+    fetch(apiUrl + '/spaces', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw Error(response.statusText);
+        }
+    })
+    .then(json => console.log('Created space: ', json.name, json.uri))
+    .catch(error => console.error('Error: ', error));}
