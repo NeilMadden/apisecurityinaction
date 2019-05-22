f92c92fa78ae5aa9e4a98fedf582cb06d25a2e13
Neil Madden <neil.madden@forgerock.com>
2019-05-22 11:02:22 +0100

    Listing 5.1 CORS filter

---
+++ natter-api/src/main/java/com/manning/apisecurityinaction/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.manning.apisecurityinaction;
+
+import spark.*;
+
+import java.util.Set;
+
+import static spark.Spark.halt;
+
+class CorsFilter implements Filter {
+    private final Set<String> allowedOrigins;
+
+    CorsFilter(Set<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    @Override
+    public void handle(Request request, Response response) {
+        var origin = request.headers("Origin");
+        if (origin != null && allowedOrigins.contains(origin)) {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Allow-Credentials",
+                    "true");
+            response.header("Vary", "Origin");
+        }
+
+        if (isPreflightRequest(request)) {
+            if (origin == null || !allowedOrigins.contains(origin)) {
+                halt(403);
+            }
+
+            response.header("Access-Control-Allow-Headers",
+                    "Content-Type, X-CSRF-Token");
+            response.header("Access-Control-Allow-Methods",
+                    "GET, POST, DELETE");
+            halt(204);
+        }
+    }
+
+    private boolean isPreflightRequest(Request request) {
+        return "OPTIONS".equals(request.requestMethod()) &&
+        request.headers().contains("Access-Control-Request-Method");
+    }
+}
