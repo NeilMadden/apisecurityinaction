bba1c0909dc3e05f0b698aa88f387b432c336de0
Neil Madden <neil.madden@forgerock.com>
2019-05-23 12:32:13 +0100

    Add the HmacTokenStore

---
+++ natter-api/src/main/java/com/manning/apisecurityinaction/token/HmacTokenStore.java
@@ -0,0 +1,59 @@
+package com.manning.apisecurityinaction.token;
+
+import spark.Request;
+
+import javax.crypto.Mac;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.*;
+
+public class HmacTokenStore implements TokenStore {
+
+    private final TokenStore delegate;
+    private final Key macKey;
+
+    public HmacTokenStore(TokenStore delegate, Key macKey) {
+        this.delegate = delegate;
+        this.macKey = macKey;
+    }
+
+    @Override
+    public String create(Request request, Token token) {
+        var tokenId = delegate.create(request, token);
+        var tag = hmac(tokenId);
+
+        return tokenId + '.' +
+                Base64.getUrlEncoder().withoutPadding()
+                    .encodeToString(tag);
+    }
+
+    private byte[] hmac(String tokenId) {
+        try {
+            var mac = Mac.getInstance("HmacSHA256");
+            mac.init(macKey);
+            return mac.doFinal(
+                    tokenId.getBytes(StandardCharsets.UTF_8));
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Optional<Token> read(Request request, String tokenId) {
+        var index = tokenId.lastIndexOf('.');
+        if (index == -1) {
+            return Optional.empty();
+        }
+        var realTokenId = tokenId.substring(0, index);
+
+        var provided = Base64.getUrlDecoder()
+                .decode(tokenId.substring(index + 1));
+        var computed = hmac(realTokenId);
+
+        if (!MessageDigest.isEqual(provided, computed)) {
+            return Optional.empty();
+        }
+
+        return delegate.read(request, realTokenId);
+    }
+}
