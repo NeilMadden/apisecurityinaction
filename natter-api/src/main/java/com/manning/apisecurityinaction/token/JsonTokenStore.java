d2cba59a2df40a579c5797ea8b1b8564a1b54730
Neil Madden <neil.madden@forgerock.com>
2019-05-24 21:04:29 +0100

    Verify the audience as a JWT.

--- natter-api/src/main/java/com/manning/apisecurityinaction/token/JsonTokenStore.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/token/JsonTokenStore.java
@@ -15,6 +15,7 @@ public class JsonTokenStore implements TokenStore {
         var json = new JSONObject();
         json.put("sub", token.username);
         json.put("exp", token.expiry.getEpochSecond());
+        json.put("aud", List.of("https://localhost:4567"));
         json.put("attrs", token.attributes);
 
         var jsonBytes = json.toString().getBytes(UTF_8);
@@ -29,7 +30,12 @@ public class JsonTokenStore implements TokenStore {
             var json = new JSONObject(new String(decoded, UTF_8));
             var expiry = Instant.ofEpochSecond(json.getInt("exp"));
             var username = json.getString("sub");
+            var audience = json.getJSONArray("aud").toList();
             var attrs = json.getJSONObject("attrs");
+
+            if (!audience.contains("https://locahost:4567")) {
+                return Optional.empty();
+            }
 
             var token = new Token(expiry, username);
             for (var key : attrs.keySet()) {
