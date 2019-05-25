eac754e65e516602ecbb90508833f9dae8510ec8
Neil Madden <neil.madden@forgerock.com>
2019-05-25 21:07:32 +0100

    Extract JWT header into a separate wrapper store

--- natter-api/src/main/java/com/manning/apisecurityinaction/token/HmacTokenStore.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/token/HmacTokenStore.java
@@ -1,6 +1,5 @@
 package com.manning.apisecurityinaction.token;
 
-import org.json.JSONObject;
 import spark.Request;
 
 import javax.crypto.Mac;
@@ -22,12 +21,6 @@ public class HmacTokenStore implements TokenStore {
     @Override
     public String create(Request request, Token token) {
         var tokenId = delegate.create(request, token);
-        var header = new JSONObject()
-                .put("alg", jwsAlgorithm(macKey))
-                .put("typ", "JWT").toString();
-        header = Base64.getUrlEncoder().withoutPadding()
-                .encodeToString(header.getBytes(UTF_8));
-        tokenId = header + '.' + tokenId;
         var tag = hmac(tokenId);
 
         return tokenId + '.' +
@@ -39,8 +32,7 @@ public class HmacTokenStore implements TokenStore {
         try {
             var mac = Mac.getInstance(macKey.getAlgorithm());
             mac.init(macKey);
-            return mac.doFinal(
-                    tokenId.getBytes(UTF_8));
+            return mac.doFinal(tokenId.getBytes(UTF_8));
         } catch (GeneralSecurityException e) {
             throw new RuntimeException(e);
         }
@@ -48,47 +40,20 @@ public class HmacTokenStore implements TokenStore {
 
     @Override
     public Optional<Token> read(Request request, String tokenId) {
-        var parts = tokenId.split("\\.");
-        if (parts.length != 3) return Optional.empty();
+        var index = tokenId.lastIndexOf('.');
+        if (index == -1) return Optional.empty();
 
-        var header = parts[0];
-        var payload = parts[1];
-        var tag = parts[2];
+        var realTokenId = tokenId.substring(0, index);
+        var tag = tokenId.substring(index + 1);
 
         var decoder = Base64.getUrlDecoder();
         var provided = decoder.decode(tag);
-        var computed = hmac(header + '.' + payload);
+        var computed = hmac(realTokenId);
 
         if (!MessageDigest.isEqual(provided, computed)) {
             return Optional.empty();
         }
 
-        var jwtHeader = new JSONObject(
-                new String(decoder.decode(header), UTF_8));
-        if (!"JWT".equals(jwtHeader.getString("typ"))) {
-            return Optional.empty();
-        }
-        if (!jwsAlgorithm(macKey).equals(jwtHeader.getString("alg"))) {
-            return Optional.empty();
-        }
-
-        return delegate.read(request, payload);
-    }
-
-    private static String jwsAlgorithm(Key key) {
-        switch (key.getAlgorithm()) {
-            case "HmacSHA256":
-            case "1.2.840.113549.2.9":
-                return "HS256";
-            case "HmacSHA384":
-            case "1.2.840.113549.2.10":
-                return "HS384";
-            case "HmacSHA512":
-            case "1.2.840.113549.2.11":
-                return "HS512";
-            default:
-                throw new IllegalStateException(
-                        "unknown algorithm: " + key.getAlgorithm());
-        }
+        return delegate.read(request, realTokenId);
     }
 }
---
+++ natter-api/src/main/java/com/manning/apisecurityinaction/token/JwtHeaderTokenStore.java
@@ -0,0 +1,50 @@
+package com.manning.apisecurityinaction.token;
+
+import org.json.JSONObject;
+import spark.Request;
+
+import java.util.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class JwtHeaderTokenStore implements TokenStore {
+
+    private final TokenStore delegate;
+    private final JSONObject header;
+
+    public JwtHeaderTokenStore(TokenStore delegate, JSONObject header) {
+        this.delegate = delegate;
+        this.header = header;
+    }
+
+    @Override
+    public String create(Request request, Token token) {
+        var tokenId = delegate.create(request, token);
+        var headerBytes = header.toString().getBytes(UTF_8);
+        return Base64.getUrlEncoder().withoutPadding()
+                .encodeToString(headerBytes) + '.' + tokenId;
+    }
+
+    @Override
+    public Optional<Token> read(Request request, String tokenId) {
+        var index = tokenId.indexOf('.');
+        if (index == -1) return Optional.empty();
+
+        var encodedHeader = tokenId.substring(0, index);
+        var realTokenId = tokenId.substring(index + 1);
+
+        var decodedHeader = Base64.getUrlDecoder()
+                .decode(encodedHeader);
+        var suppliedHeader = new JSONObject(
+                new String(decodedHeader, UTF_8));
+
+        for (var expected : this.header.keySet()) {
+            if (!Objects.equals(this.header.get(expected),
+                    suppliedHeader.get(expected))) {
+                return Optional.empty();
+            }
+        }
+
+        return delegate.read(request, realTokenId);
+    }
+}
