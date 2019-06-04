de2d8f839534822e98da214179cc627b8f3d94de
Neil Madden <neil.madden@forgerock.com>
2019-06-04 12:36:03 +0100

    Implement token whitelisting for JWTs

--- natter-api/src/main/java/com/manning/apisecurityinaction/token/JwtTokenStore.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/token/JwtTokenStore.java
@@ -12,14 +12,21 @@ import java.util.*;
 public class JwtTokenStore implements SecureTokenStore {
 
     private final SecretKey encKey;
+    private final DatabaseTokenStore tokenWhitelist;
 
-    public JwtTokenStore(SecretKey encKey) {
+    public JwtTokenStore(SecretKey encKey,
+                         DatabaseTokenStore tokenWhitelist) {
         this.encKey = encKey;
+        this.tokenWhitelist = tokenWhitelist;
     }
 
     @Override
     public String create(Request request, Token token) {
+        var whitelistToken = new Token(token.expiry, token.username);
+        var jwtId = tokenWhitelist.create(request, whitelistToken);
+
         var claimsBuilder = new JWTClaimsSet.Builder()
+                .jwtID(jwtId)
                 .subject(token.username)
                 .audience("https://localhost:4567")
                 .expirationTime(Date.from(token.expiry));
@@ -47,11 +54,17 @@ public class JwtTokenStore implements SecureTokenStore {
             jwt.decrypt(decryptor);
 
             var claims = jwt.getJWTClaimsSet();
+            var jwtId = claims.getJWTID();
+            if (tokenWhitelist.read(request, jwtId).isEmpty()) {
+                return Optional.empty();
+            }
+
             if (!claims.getAudience().contains("https://localhost:4567")) {
                 return Optional.empty();
             }
             var expiry = claims.getExpirationTime().toInstant();
             var subject = claims.getSubject();
+
             var token = new Token(expiry, subject);
             var ignore = Set.of("exp", "sub", "aud");
             for (var attr : claims.getClaims().keySet()) {
@@ -67,6 +80,16 @@ public class JwtTokenStore implements SecureTokenStore {
 
     @Override
     public void revoke(Request request, String tokenId) {
-        // TODO
+        try {
+            var jwt = EncryptedJWT.parse(tokenId);
+
+            var decryptor = new DirectDecrypter(encKey);
+            jwt.decrypt(decryptor);
+            var claims = jwt.getJWTClaimsSet();
+
+            tokenWhitelist.revoke(request, claims.getJWTID());
+        } catch (ParseException | JOSEException e) {
+            throw new IllegalArgumentException("invalid token", e);
+        }
     }
 }
