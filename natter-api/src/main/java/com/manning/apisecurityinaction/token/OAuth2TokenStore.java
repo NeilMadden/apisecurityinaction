384e4e09c3072539b641c4df1c7e4adc409690e6
Neil Madden <neil.madden@forgerock.com>
2019-09-11 11:15:31 +0100

    Secure HTTPS client settings

--- natter-api/src/main/java/com/manning/apisecurityinaction/token/OAuth2TokenStore.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/token/OAuth2TokenStore.java
@@ -1,10 +1,12 @@
 package com.manning.apisecurityinaction.token;
 
-import java.io.IOException;
+import javax.net.ssl.*;
+import java.io.*;
 import java.net.*;
 import java.net.http.*;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.security.*;
 import java.time.Instant;
 import java.util.*;
 
@@ -29,7 +31,42 @@ public class OAuth2TokenStore implements SecureTokenStore {
         this.authorization = "Basic " + Base64.getEncoder()
                 .encodeToString(credentials.getBytes(UTF_8));
 
-        this.httpClient = HttpClient.newHttpClient();
+        var sslParams = new SSLParameters();
+        sslParams.setProtocols(new String[] {
+                // TLS 1.3 cipher suites
+                "TLS_AES_128_GCM_SHA256",
+                "TLS_AES_256_GCM_SHA384",
+                "TLS_CHACHA20_POLY1305_SHA256",
+
+                // TLS 1.2 cipher suites
+                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+                "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+        });
+        sslParams.setUseCipherSuitesOrder(true);
+        sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+
+        try {
+            var trustedCerts = KeyStore.getInstance("PKCS12");
+            trustedCerts.load(
+                    new FileInputStream("as.example.com.ca.p12"),
+                    "changeit".toCharArray());
+            var tmf = TrustManagerFactory.getInstance("PKIX");
+            tmf.init(trustedCerts);
+            var sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+
+            this.httpClient = HttpClient.newBuilder()
+                    .sslParameters(sslParams)
+                    .sslContext(sslContext)
+                    .build();
+
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
