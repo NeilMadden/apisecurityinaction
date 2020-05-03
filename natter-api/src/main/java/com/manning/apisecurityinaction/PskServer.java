e7b926a37b148a5893b890c38d5010a8d0accfbd
Neil Madden <neil.madden@forgerock.com>
2020-05-03 13:46:08 +0100

    Add device database

--- natter-api/src/main/java/com/manning/apisecurityinaction/PskServer.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/PskServer.java
@@ -1,17 +1,25 @@
 package com.manning.apisecurityinaction;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import org.bouncycastle.tls.*;
+import org.bouncycastle.tls.crypto.impl.bc.BcTlsCrypto;
+import org.dalesbred.Database;
+import org.dalesbred.annotation.DalesbredInstantiator;
+import org.h2.jdbcx.JdbcConnectionPool;
+import software.pando.crypto.nacl.*;
+
 import java.io.FileInputStream;
 import java.net.*;
 import java.security.*;
-import org.bouncycastle.tls.*;
-import org.bouncycastle.tls.crypto.impl.bc.BcTlsCrypto;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class PskServer {
     public static void main(String[] args) throws Exception {
         var psk = loadPsk(args[0].toCharArray());
+        var encryptionKey = SecretBox.key();
+        var deviceDb = createDatabase(encryptionKey, psk);
         var crypto = new BcTlsCrypto(new SecureRandom());
-        var server = new PSKTlsServer(crypto, getIdentityManager(psk)) {
+        var server = new PSKTlsServer(crypto, getIdentityManager(deviceDb, encryptionKey)) {
             @Override
             protected ProtocolVersion[] getSupportedVersions() {
                 return ProtocolVersion.DTLSv12.only();
@@ -47,7 +55,8 @@ public class PskServer {
         }
     }
 
-    static TlsPSKIdentityManager getIdentityManager(byte[] psk) {
+    static TlsPSKIdentityManager getIdentityManager(
+            Database deviceDb, Key decryptionKey) {
         return new TlsPSKIdentityManager() {
             @Override
             public byte[] getHint() {
@@ -56,7 +65,12 @@ public class PskServer {
 
             @Override
             public byte[] getPSK(byte[] identity) {
-                return psk;
+                var device = deviceDb.findUnique(Device.class,
+                        "SELECT device_id, psk_id, encrypted_psk " +
+                                "FROM devices " +
+                                "WHERE psk_id = ?", identity);
+                System.out.println("Loaded PSK from client device: " + device.deviceId);
+                return device.encryptedPsk.decrypt(decryptionKey);
             }
         };
     }
@@ -65,5 +79,34 @@ public class PskServer {
         var keyStore = KeyStore.getInstance("PKCS12");
         keyStore.load(new FileInputStream("keystore.p12"), password);
         return keyStore.getKey("aes-key", password).getEncoded();
+    }
+
+    static Database createDatabase(Key encryptionKey, byte[] exampleDevicePsk) {
+        var pool = JdbcConnectionPool.create("jdbc:h2:mem:psk", "psk", "dummy");
+        var database = Database.forDataSource(pool);
+        database.update("CREATE TABLE devices(" +
+                "device_id VARCHAR(255) PRIMARY KEY," +
+                "psk_id BINARY(64) NOT NULL," +
+                "encrypted_psk VARCHAR(1024) NOT NULL);");
+        database.update("CREATE UNIQUE INDEX psk_id_idx ON devices(psk_id);");
+
+        var encryptedPsk = SecretBox.encrypt(encryptionKey, exampleDevicePsk).toString();
+        database.update("INSERT INTO devices(device_id, psk_id, encrypted_psk) VALUES (?, ?, ?)",
+                "test", Crypto.hash(exampleDevicePsk), encryptedPsk);
+
+        return database;
+    }
+
+    public static class Device {
+        final String deviceId;
+        final byte[] pskId;
+        final SecretBox encryptedPsk;
+
+        @DalesbredInstantiator
+        public Device(String deviceId, byte[] pskId, String encryptedPsk) {
+            this.deviceId = deviceId;
+            this.pskId = pskId;
+            this.encryptedPsk = SecretBox.fromString(encryptedPsk);
+        }
     }
 }
