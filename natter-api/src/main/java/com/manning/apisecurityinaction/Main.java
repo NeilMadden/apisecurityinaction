6800c4b4fc6f92b73dee8a45750b30ec118f4647
Neil Madden <neil.madden@forgerock.com>
2020-07-29 22:34:50 +0100

    Update security headers for latest OWASP guidance

--- natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
@@ -34,11 +34,11 @@ public class Main {
         }));
 
         afterAfter((request, response) -> {
-            response.type("application/json; charset=utf-8");
+            response.type("application/json;charset=utf-8");
             response.header("X-Content-Type-Options", "nosniff");
-            response.header("X-Frame-Options", "deny");
-            response.header("X-XSS-Protection", "1; mode=block");
-            response.header("Cache-Control", "private, max-age=0");
+            response.header("X-Frame-Options", "DENY");
+            response.header("X-XSS-Protection", "0");
+            response.header("Cache-Control", "no-store");
             response.header("Content-Security-Policy",
                     "default-src 'none'; frame-ancestors 'none'; sandbox");
             response.header("Server", "");
